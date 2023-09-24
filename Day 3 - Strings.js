9330f785ae49ccf5f5fadebd6741d2e442391c53
W1NTXR <92108800+W1NTXR@users.noreply.github.com>
2023-09-24 23:56:51 +0530

    Day 4

--- Day 3 - Strings.js
+++ Day 3 - Strings.js
@@ -1,2 +1,11 @@
-let nae = "Harry"
-console.log(nae.length())
+let nae = "Harry" // can either be single quoted or double quoted
+console.log(nae.length)
+let naenae = 'just lift bro'
+console.log(naenae)
+/*string interpolation */ let sent =  `${nae} , ${naenae}` //template literals use backtick instead of quotes
+//we can also use single or double quotes in template literals and variables too just like we did above this is called string interpolation
+console.log(sent)
+
+let x = 'Your\'s' // another way of using a quote in a string with the help of escape sequence '\'
+console.log(x)
+console.log(x.length)
---
+++ Day 4-String methods.js
@@ -0,0 +1,2 @@
+let str = "lessgo"
+console.log(str.toUpperCase())
