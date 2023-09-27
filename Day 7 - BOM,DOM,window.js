d0ba88b4acc8627aff5e9cf5d264e73f33ecbc27
W1NTXR <92108800+W1NTXR@users.noreply.github.com>
2023-09-27 23:53:12 +0530

    Day 7

---
+++ Day 7 - BOM,DOM,window.js
@@ -0,0 +1,10 @@
+//DOM stands for document object model
+//BOM stands for Browser Object modl
+//windowObject
+
+console.log(window) //gives information on window object
+window.console.log(window)
+console.log(document)
+//document is a javascript representation on html webpage
+console.log(document.body)
+document.body.style.background = "red"
