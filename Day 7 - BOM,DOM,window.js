410a13cffc48df63ae23a8db974610daca9284bb
W1NTXR <92108800+W1NTXR@users.noreply.github.com>
2023-09-28 21:37:23 +0530

    Day 8

--- Day 7 - BOM,DOM,window.js
+++ Day 7 - BOM,DOM,window.js
@@ -8,3 +8,8 @@ console.log(document)
 //document is a javascript representation on html webpage
 console.log(document.body)
 document.body.style.background = "red"
+//window object represents the browsers window and provides methods to control it. It is a global object
+//BOM stands for Browser Object Model and represents additional objects provided by the browser(host environment)
+//basically everything other than document
+//alert, prompt and confirm are also part of BOMs
+location.href = "saaransh.netlify.app"
